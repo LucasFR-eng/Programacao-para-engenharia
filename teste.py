6e51145ada528275b1d995f384ba433e3ef2db2e
Lucas Fernandes Rovaris <lucasfrovaris17@gmail.com>
2025-06-12 09:26:31 -0300

    s

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 def m(a,b):
-    resultado = (a + b) / 2
-    return resultado
+    s = (a + b) / 2
+    return s
 x = int(input("Digite o primeiro numero:"))
 y = int(input("Digite o segundo numero:"))
 resposta = m(x,y)
